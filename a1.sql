e708f83e0dbae1f598d4ea496449f9d7a311c04d
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-12-07 15:33:31 +0530

    commit

--- a1.sql
+++ a1.sql
@@ -11,4 +11,6 @@ INSERT INTO supplier(SNO,SNAME,STATUS,CITY) VALUES
 ('S3','BLAKE',50,'ATHENS'),
 ('S4','JOHN',10,'LONDON');
 
-SELECT * FROM supplier;
+SELECT * FROM supplier where CITY=='LONDON';
+
+DROP TABLE supplier;
